dcf4e8c60c1cb905e57cf283310120edd260809b
klimenteen <kpetruchkin@gmail.com>
2022-04-01 17:59:33 +0700

    first page

---
+++ src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  Navbar from '../components/NavBar'
+import Video from '../components/Video'
+import '../index'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Video />
+    </div>
+  )
+}
+
+export default Home
